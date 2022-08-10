0780ecd7b70cf5a948d6dc922a1bea76fec00582
fugui <szfugui@gmail.com>
2022-08-10 14:47:09 +0800

    add some comments

--- src/bin/learn01.rs
+++ src/bin/learn01.rs
@@ -4,6 +4,7 @@
 fn main() {
     let mut facts = [0, 1, 3, 0, -3, 4, 9, 0, 0, 2, 1, 0, 5];
 
+    // Move all none zero to the end of array
     let mut none_zero = facts.len() - 1;
     for i in (0..facts.len()).rev() {
         if facts[i] != 0 {
@@ -11,6 +12,8 @@ fn main() {
             none_zero -= 1;
         }
     }
+
+    // fill zero at the beginning
     for i in 0..=none_zero {
         facts[i] = 0;
     }
