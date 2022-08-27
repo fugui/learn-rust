18e61b5ec1ef5bd382f430564b7aa935ba5709dc
fugui <szfugui@gmail.com>
2022-08-27 14:49:08 +0800

    refactor with generic

--- src/bin/learn04.rs
+++ src/bin/learn04.rs
@@ -1,32 +1,40 @@
+use std::fmt::Display;
+
 /**
  * From document : https://rust-unofficial.github.io/too-many-lists/second.html
  */
 #[derive(Debug)]
-struct Node {
-    value: i32,
-    next: Link,
+struct Node<T> {
+    value: T,
+    next: Link<T>,
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
 #[derive(Debug)]
-struct LinkedList {
-    head: Link,
+struct LinkedList<T>
+where
+    T: Display,
+{
+    head: Link<T>,
 }
 
-impl LinkedList {
+impl<T> LinkedList<T>
+where
+    T: Display,
+{
     fn new() -> Self {
         LinkedList { head: None }
     }
 
-    fn push(&mut self, value: i32) {
+    fn push(&mut self, value: T) {
         self.head = Some(Box::new(Node {
             value: value,
             next: self.head.take(), //std::mem::replace(&mut self.head, None),
         }))
     }
 
-    fn pop(&mut self) -> Option<i32> {
+    fn pop(&mut self) -> Option<T> {
         /*match self.head.take() {
             //std::mem::replace(&mut self.head, None) {
             Option::None => None,
@@ -42,7 +50,10 @@ impl LinkedList {
     }
 }
 
-impl Drop for LinkedList {
+impl<T> Drop for LinkedList<T>
+where
+    T: Display,
+{
     fn drop(&mut self) {
         let mut current = self.head.take(); //std::mem::replace(&mut self.head, None);
         while let Some(mut node) = current {
